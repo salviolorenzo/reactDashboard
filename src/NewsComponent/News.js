d408d3312138d70b53ea018f39d6633ba54d0f60
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-12-10 21:19:23 -0500

    adds news nav bar and another fetch

--- src/NewsComponent/News.js
+++ src/NewsComponent/News.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Keys from '../config';
 import NewsList from './NewsList';
+import NewsNav from './NewsNav';
 
 class News extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      stories: []
+      stories: [],
+      category: ''
     };
   }
 
@@ -23,10 +25,31 @@ class News extends Component {
       });
   }
 
+  _handleClick(item) {
+    item = item.toLowerCase();
+    console.log(item);
+    this.setState({
+      category: `${item}`
+    });
+    fetch(
+      `https://newsapi.org/v2/top-headlines?country=us&category=${
+        this.state.category
+      }&apiKey=${Keys.newsKey}`
+    )
+      .then(r => r.json())
+      .then(results => {
+        let newArray = results.articles.slice(0, 5);
+        this.setState({
+          stories: newArray
+        });
+      });
+  }
+
   render() {
     return (
       <div className='news'>
         <h2>Today's News</h2>
+        <NewsNav handleClick={this._handleClick.bind(this)} />
         <NewsList items={this.state.stories} />
       </div>
     );
---
+++ src/NewsComponent/NewsNav.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const categories = [
+  'Business',
+  'Entertainment',
+  'General',
+  'Health',
+  'Science',
+  'Sports',
+  'Technology'
+];
+
+function NewsNav(props) {
+  return (
+    <ul className='news-nav'>
+      {categories.map((item, index) => {
+        return (
+          <li
+            key={index}
+            onClick={() => {
+              props.handleClick(item);
+            }}
+          >
+            {item}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default NewsNav;
