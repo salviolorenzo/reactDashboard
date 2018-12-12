d6f80207093fc12958cc1a5fff0acb7dd2f6f8f7
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-12-12 14:28:39 -0500

    sets background based on weather

--- src/WeatherComponent/Weather.js
+++ src/WeatherComponent/Weather.js
@@ -3,38 +3,11 @@ import Info from './Info';
 import Keys from '../config';
 import Draggable from 'react-draggable';
 
-function createLocationObject(object) {
-  let location = {
-    lat: object.coords.latitude.toFixed(),
-    long: object.coords.longitude.toFixed()
-  };
-  console.log(location);
-  fetch(
-    `http://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${
-      location.long
-    }&type=accurate&apikey=${Keys.OWKey}`
-  )
-    .then(r => r.json())
-    .then(weather => {
-      let temp = (((weather.main.temp - 273.15) * 9) / 5 + 32).toFixed(2);
-      this.setState({
-        name: weather.name,
-        temp: `${temp} °F`,
-        humidity: `${weather.main.humidity}%`,
-        wind: `${weather.wind.speed} MPH`,
-        iconUrl: `http://openweathermap.org/img/w/${
-          weather.weather[0].icon
-        }.png`
-      });
-    });
-  return location;
-}
-
 class Weather extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
+      desc: '',
       temp: '',
       humidity: '',
       wind: '',
@@ -42,8 +15,14 @@ class Weather extends Component {
     };
   }
 
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition(createLocationObject.bind(this));
+  componentWillReceiveProps(props) {
+    this.setState({
+      desc: props.desc,
+      temp: props.temp,
+      humidity: props.humidity,
+      wind: props.wind,
+      iconUrl: props.url
+    });
   }
 
   render() {
