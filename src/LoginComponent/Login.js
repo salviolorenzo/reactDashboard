dbcdbab6e8832403659f858f42c55c8f18296318
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-12-16 16:02:59 -0500

    adds login page styles

--- src/LoginComponent/Login.js
+++ src/LoginComponent/Login.js
@@ -4,13 +4,9 @@ function Login(props) {
   return (
     <form action='/login' method='POST' className='homeForm'>
       <label>Username: </label>
-      <br />
       <input type='text' name='username' />
-      <br />
       <label>Password: </label>
-      <br />
       <input type='password' name='password' />
-      <br />
       <input type='submit' value='Log in' />
     </form>
   );
--- src/LoginComponent/Register.js
+++ src/LoginComponent/Register.js
@@ -4,21 +4,13 @@ function Register(props) {
   return (
     <form action='/register' method='POST' className='homeForm'>
       <label>Name: </label>
-      <br />
       <input type='text' name='name' />
-      <br />
       <label>Email: </label>
-      <br />
       <input type='email' name='email' />
-      <br />
       <label>Username: </label>
-      <br />
       <input type='text' name='username' />
-      <br />
       <label>Password: </label>
-      <br />
       <input type='password' name='password' />
-      <br />
       <input type='submit' value='Log in' />
     </form>
   );
